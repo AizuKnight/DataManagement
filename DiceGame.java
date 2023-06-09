93faef58b95c6128df56ae99cc043c3ca319f558
AizuKnight <shun@kyuuchan.com>
2023-06-09 12:02:10 +0900

    Won/lost message added

--- DiceGame.java
+++ DiceGame.java
@@ -13,5 +13,7 @@ class DiceGame {
         int die2 = random.nextInt(6) + 1;
         System.out.println("Die 2: " + die2);
         System.out.println("Total value: " + (die1+die2));
+        if(die1+die2 >= 7) System.out.println("You won!");
+        else System.out.println("You lost!");
     }
 }
